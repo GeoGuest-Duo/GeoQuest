e1f2f655ef54c7ebffa455f10886caf75cc94e1a
Ayan-Derie1 <Ayanderie16@gmail.com>
2026-03-10 22:54:07 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Our fisrt project togther yeye!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+export default App;
