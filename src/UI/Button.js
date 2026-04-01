c0459bb6b1ad6f38983bd3eda6f53c614c0968b5
SCheriyamParambil <shaniyacp@outlook.com>
2026-04-01 17:23:22 +0100

    Authentication set up (#2)
    * Added stack navigation and initial LoginScreen route
    
    * Built  a simple login UI
    
    - added input fields for username and password
    - created a button using a reusable button component
    - Created a simple handler for validation
    
    * Implemented login authentication with  API and navigation flow
    
    - Added user input validation
    - Implemented login functionality
    - Integrated API to fetch user data

---
+++ src/UI/Button.js
@@ -0,0 +1,55 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Selector from './Selector';
+
+export const Button = ({label, icon, onClick, styleButton, styleLabel}) => {
+// Initialisations ---------------------
+// State -------------------------------
+// Handlers ----------------------------
+// View --------------------------------
+    return (
+        <Selector
+            onPress={onClick} 
+            style={[styles.button, styleButton]}
+            pressedStyle={styles.pressedButton}
+        >
+            {icon ? icon : null}
+            <Text style={[styles.label, styleLabel]}>{label}</Text>
+        </Selector>
+    );
+};
+
+export const ButtonTray = ({children}) => {
+// Initialisations ---------------------
+// State -------------------------------
+// Handlers ----------------------------
+// View --------------------------------
+    return <View style={styles.buttonTray}>{children}</View>   
+};
+
+const styles = StyleSheet.create({
+    buttonTray: {
+        flexDirection: 'row',
+        gap: 15,
+    },
+    button: {
+        minHeight: 50,
+        borderWidth: 1,
+        borderRadius: 7,
+        borderColor: 'grey',
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        gap: 5,
+    },
+    label: {
+        fontSize: 16,
+    },
+    pressedButton: {
+        backgroundColor: 'azure',
+        elevation: 5,
+    },
+});
+
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { Alert, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button } from "../UI/Button";
+import API from "../API/API";
+
+const LoginScreen = ({ navigation }) => {
+    // Initialisations ---------------------
+    const usersEndpoint = "https://mark0s.com/geoquest/v1/api/users?key=16gv8f";
+
+    // State -------------------------------
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Handlers ----------------------------
+    const handleLogin = async () => {
+        // To validate user input
+        if (!username || !password) {
+            Alert.alert("Error", "Please fill all the required fields");
+            return;
+        }
+
+        // To fetch user details
+        const response = await API.get(usersEndpoint);
+
+        if (!response.isSuccess) {
+            Alert.alert("Error", response.message);
+            return;
+        }
+
+        // Checks the userInput against user data
+        const users = response.result;
+        const matchedUser = users.find((user) =>
+            user.UserUsername === username &&
+            user.UserPassword === password
+        );
+
+        if (matchedUser) {
+            navigation.replace("HomeScreen", { user: matchedUser });
+            console.log("Login success");
+        } else {
+            Alert.alert("Login Failed", "Invalid username or password")
+        }
+    };
+
+    // View --------------------------------
+    return (
+        <View style={styles.container}>
+            <TextInput
+                placeholder="Username"
+                value={username}
+                onChangeText={setUsername}
+                style={styles.inputField}
+            />
+
+            <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={true}
+                style={styles.inputField}
+            />
+
+            <Button label="Login" onClick={handleLogin} />
+            <Pressable
+                style={styles.signUpText}
+                onPress={ () => navigation.navigate("SignupScreen") }
+            >
+                <Text>Don't have an account? Sign Up</Text>
+            </Pressable>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    inputField: {
+        borderWidth: 1,
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    signUpText: {
+        padding: 15,
+    }
+});
+
+export default LoginScreen;
