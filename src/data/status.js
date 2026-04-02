e55d987bb7eeb0c1ad780b40aa04305dcccdb815
Ayan-Derie1 <Ayanderie16@gmail.com>
2026-04-02 14:10:38 +0100

    Setting up the Access point for the API

---
+++ src/data/status.js
@@ -0,0 +1,33 @@
+const status = [
+  {
+    StatusID: 1,
+    StatusName: "Pending",
+    StatusOrder: 1,
+  },
+  {
+    StatusID: 2,
+    StatusName: "Started",
+    StatusOrder: 2,
+  },
+  {
+    StatusID: 3,
+    StatusName: "Paused",
+    StatusOrder: 3,
+  },
+  {
+    StatusID: 4,
+    StatusName: "Discontinued",
+    StatusOrder: 4,
+  },
+  {
+    StatusID: 5,
+    StatusName: "Finished",
+    StatusOrder: 5,
+  },
+  {
+    StatusID: 6,
+    StatusName: "Cancelled",
+    StatusOrder: 6,
+  },
+];
+export default status;
