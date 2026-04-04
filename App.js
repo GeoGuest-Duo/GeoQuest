02d17e2691f8fdaa31ac1912912490f9f3d6aded
SCheriyamParambil <shaniyacp@outlook.com>
2026-04-04 03:02:31 +0100

    Implemented persistent login and conditional navigation
    - Created a reusable hook to persist loggedinUser data
    - Added conditional rendering of navigation screens based on login state

--- App.js
+++ App.js
@@ -4,34 +4,50 @@ import LoginScreen from "./src/screens/LoginScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 import EventScreen from "./src/screens/EventScreen";
+import useStore from "./src/store/useStore";
 
 const Stack = createNativeStackNavigator();
 
 export const App = () => {
+  // Initialisations ---------------------
+  const loggedinUserKey = "loggedinUser";
+
+  // State -------------------------------
+  const [loggedinUser, saveLoggedinUser] = useStore(loggedinUserKey, null) // key = loggedinUser, value = null
+
+  // View --------------------------------
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="EventScreen">
-        <Stack.Screen
-          name="LoginScreen"
-          component={LoginScreen}
-          options={{ title: "Login" }}
-        />
+      <Stack.Navigator initialRouteName="LoginScreen">
+        {loggedinUser === null ? (
+          // <>...</> groups multiple <Stack.Screen> components together
+          <>
+            <Stack.Screen
+              name="LoginScreen"
+              component={LoginScreen}
+              options={{ title: "Login" }}
+            />
 
-        <Stack.Screen
-          name="SignupScreen"
-          component={SignupScreen}
-          options={{ title: "Create an account" }}
-        />
-
-        <Stack.Screen
-          name="HomeScreen"
-          component={HomeScreen}
-          options={{ title: "Welcome to GeoQuest" }}
-        />
-        <Stack.Screen
-          name="EventScreen"
-          component={EventScreen}
-          options={{ title: "Events" }}></Stack.Screen>
+          <Stack.Screen
+            name="SignupScreen"
+            component={SignupScreen}
+            options={{ title: "Create an account" }}
+          />
+        </>  
+        ) : (
+        <>
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{ title: "Welcome to GeoQuest" }}s
+          />
+          <Stack.Screen
+            name="EventScreen"
+            component={EventScreen}
+            options={{ title: "Events" }}>
+          </Stack.Screen>
+        </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
