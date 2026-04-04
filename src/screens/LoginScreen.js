ffb7edd478e9a231b2025dfb187d5f2edcbe5cc5
SCheriyamParambil <shaniyacp@outlook.com>
2026-04-04 01:26:26 +0100

    Created a reusable hook to fetch data from the API

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,99 +1,89 @@
-import { useState } from "react";
-import {
-  Alert,
-  Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { Alert, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import { Button } from "../UI/Button";
-import API from "../API/API";
+import { useState } from "react";
+import useLoad from "../API/useLoad";
 
 const LoginScreen = ({ navigation }) => {
-  // Initialisations ---------------------
-  //The right Endpoint
-  const usersEndpoint = "https://mark0s.com/geoquest/v1/api/users?key=16gv8f";
+    // Initialisations ---------------------
+    const usersEndpoint = "https://mark0s.com/geoquest/v1/api/users?key=16gv8f";
 
-  // State -------------------------------
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+    // State -------------------------------
+    const [users, setUsers, isUsersLoading, loadUsers] = useLoad(usersEndpoint)
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-  // Handlers ----------------------------
-  const handleLogin = async () => {
-    // To validate user input
-    if (!username || !password) {
-      Alert.alert("Error", "Please fill all the required fields");
-      return;
-    }
+    // Handlers ----------------------------
+    const handleLogin = async () => {
+        // To validate user input
+        if (!username || !password) {
+        Alert.alert("Error", "Please fill all the required fields");
+        return;
+        }
 
-    // To fetch user details
-    const response = await API.get(usersEndpoint);
+        // Checks if the user data is loaded
+        if (isUsersLoading) {
+            Alert.alert("Please wait", "User data is loading")
+            return
+        }
 
-    if (!response.isSuccess) {
-      Alert.alert("Error", response.message);
-      return;
-    }
+        // Checks the userInput against user data
+        const matchedUser = users.find(
+        (user) =>
+            user.UserUsername === username.trim() &&
+            user.UserPassword === password.trim()
+        );
 
-    // Checks the userInput against user data
-    const users = response.result;
-    const matchedUser = users.find(
-      (user) =>
-        user.UserUsername === username.trim() &&
-        user.UserPassword === password.trim(),
+        if (matchedUser) {
+            console.log("Login success");
+            navigation.replace("HomeScreen", { user: matchedUser });
+        } else {
+        Alert.alert("Login Failed", "Invalid username or password");
+        }
+    };
+
+    // View --------------------------------
+    return (
+        <View style={styles.container}>
+        <TextInput
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+            style={styles.inputField}
+        />
+
+        <TextInput
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={true}
+            style={styles.inputField}
+        />
+
+        <Button label="Login" onClick={handleLogin} />
+        <Pressable
+            style={styles.signUpText}
+            onPress={() => navigation.navigate("SignupScreen")}>
+            <Text>Don't have an account? Sign Up</Text>
+        </Pressable>
+        </View>
     );
-
-    if (matchedUser) {
-      navigation.navigate("HomeScreen", { user: matchedUser });
-      console.log("Login success");
-    } else {
-      Alert.alert("Login Failed", "Invalid username or password");
-    }
-  };
-
-  // View --------------------------------
-  return (
-    <View style={styles.container}>
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-        style={styles.inputField}
-      />
-
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry={true}
-        style={styles.inputField}
-      />
-
-      <Button label="Login" onClick={handleLogin} />
-      <Pressable
-        style={styles.signUpText}
-        onPress={() => navigation.navigate("SignupScreen")}>
-        <Text>Don't have an account? Sign Up</Text>
-      </Pressable>
-    </View>
-  );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  inputField: {
-    borderWidth: 1,
-    padding: 10,
-    marginBottom: 10,
-    borderRadius: 5,
-  },
-  signUpText: {
-    padding: 15,
-  },
-});
+    const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    inputField: {
+        borderWidth: 1,
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    signUpText: {
+        padding: 15,
+    },
+    });
 
-export default LoginScreen;
+    export default LoginScreen;
