e1c40b652b5b41ccd58b197d0ef6c204070ae449
SCheriyamParambil <shaniyacp@outlook.com>
2026-04-05 22:08:15 +0100

    Cache navigation screen (#12)
    * Implemented CacheNavigationScreen with user location, map view, cache marker
    
    - Integrated Expo location to fetch user's current location
    - Displayed MapView
    - Added cache marker using selected cache coordinates
    - Implemented mapRef with coordinates to show both user and cache
    
    * Added calculation logic to calculate the distance  between user and cache
    
    - Implemented Haversine formula to calculate distance in kilometres
    - Displayed the calculated distance on the screen
    - Distance can also be used to do proximity-based  cache logging
    
    * Added an information tile and navigation features
    
    - Added Polyline to show route from user to cache
    - Added a bottom tile with cache details
    - Implemented external navigation with Google Maps using Linking API
    - Created a button to open maps

--- src/screens/homeTab/CacheNavigationScreen.js
+++ src/screens/homeTab/CacheNavigationScreen.js
@@ -1,15 +1,222 @@
-import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from "react";
+import * as Location from "expo-location";      // Gets the user's GPS location
+import MapView, { Marker, Polyline } from "react-native-maps";
+import { Button } from '../../UI/Button';
+import { Linking } from 'react-native';
 
-const CacheNavigationScreen = () => {
+
+const calculateDistanceInKm = (startLat, startLong, endLat, endLong) => {
+    // GPS coordinates are in degrees - Js math functions need radians
+    const toRadians = (value) => (value * Math.PI) / 180;
+
+    // Stores earth's radius in KMs
+    const earthRadius = 6371;
+
+    // Calculates difference between to points 
+    const diffInLat = toRadians(endLat - startLat);
+    const diffInLong = toRadians(endLong - startLong);
+
+    // Using the Haversine formula - Calculates how curved distance works on earth 
+    const a =
+    Math.sin(diffInLat / 2) * Math.sin(diffInLat / 2) +
+    Math.cos(toRadians(startLat)) *
+    Math.cos(toRadians(endLat)) *
+    Math.sin(diffInLong / 2) *
+    Math.sin(diffInLong / 2);
+    
+    // Converts the value into an angle that represents distance on a sphere
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    // earthRadius x calculated angle
+    return earthRadius * c;
+};
+
+const CacheNavigationScreen = ({navigation, route}) => {
     // Initialisations ---------------------
+    const { cache } = route.params;
+
     // State -------------------------------
+    const [location, setLocation] = useState(null);     // Store the user's current location
+    const [isLoadingLocations, setIsLoadingLocations] = useState(true);
+    const [errorMessage, setErrorMessage] = useState("");   // Store any error message
+    const mapRef = useRef(null);    // Creates a reference to the map
+
+    // Effect ------------------------------
+    // Request location permission and get current GPS location
+    useEffect(() => {
+        const getLocation = async () => {
+            const { status } = await Location.requestForegroundPermissionsAsync();
+    
+            if (status !== "granted") {
+                setErrorMessage("Location permission denied.");
+                return;
+            }
+    
+            const loc = await Location.getCurrentPositionAsync({});
+            setLocation(loc.coords);
+            setIsLoadingLocations(false);
+         };
+    
+        getLocation();
+    }, []);
+    
+
+    // To pinpoint both user location and cache location on the map
+    useEffect(() => {
+        // if location, cache or map is not loaded or ready yet, STOP
+        if (!location || !cache || !mapRef.current) return
+
+        // adjust view include all these coordinates
+        mapRef.current.fitToCoordinates(
+            [
+                {
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                },
+                {
+                    latitude: cache.CacheLatitude,
+                    longitude: cache.CacheLongitude,
+                },
+            ],
+            {
+                edgePadding: { top: 100, right: 50, bottom: 100, left: 50 },
+                animated: true,     // makes map move smoothly
+            }
+        );
+    }, [location]) 
+    
     // Handlers ----------------------------
+    // If location exists, calculate the distance 
+    const distanceInKm = location
+        ? calculateDistanceInKm(
+            location.latitude,
+            location.longitude,
+            cache.CacheLatitude,
+            cache.CacheLongitude,
+        )
+        : 0;
+    
+    const openExternalNavigation = () => {
+        const lat = Number(cache.CacheLatitude);
+        const long = Number(cache.CacheLongitude);
+
+        const url = `https://www.google.com/maps/dir/?api=1&destination=${lat},${long}`;
+        Linking.openURL(url);
+    }
+    
+    // Loading / error states --------------
+    if (errorMessage) {
+        return (
+        <View style={styles.centerContainer}>
+            <Text>{errorMessage}</Text>
+            </View>
+        );
+    }
+      
+    if (!location || isLoadingLocations) {
+        return (
+            <View style={styles.centerContainer}>
+              <ActivityIndicator size="large" />
+              <Text style={styles.loadingText}>Loading map...</Text>
+            </View>
+        );
+    }
+    
     // View --------------------------------
     return (
-        <Text>Cache navigation screen</Text>
+        <View style={styles.container}>
+            <MapView
+                ref={mapRef}
+                style={styles.map}
+                showsUserLocation={true} // shows user location in blue
+            >
+                {/* Marks the cache location */}
+                <Marker
+                    coordinate={{
+                        latitude: Number(cache.CacheLatitude),
+                        longitude: Number(cache.CacheLongitude),
+                    }}
+                    title={cache.CacheName}
+                    description={cache.CacheDescription}
+                />
+
+                {/* To connect the two points with a line*/}
+                <Polyline
+                    coordinates={[
+                        {
+                            latitude: location.latitude,
+                            longitude: location.longitude,
+                        },
+                        {
+                            latitude: cache.CacheLatitude,
+                            longitude: cache.CacheLongitude,
+                        },
+                    ]}
+                    strokeWidth={4}
+                    strokeColor='blue'
+                />
+            </MapView>
+            <View style={styles.cacheTile}>
+                <Text style={styles.cacheTitle}>{cache.CacheName}</Text>
+                <Text style={styles.detailsText}>Distance: {distanceInKm.toFixed(2)} KM</Text>
+                <Text style={styles.detailsText}>Clue: {cache.CacheClue}</Text>
+                
+                <View style={styles.buttonContainer}>
+                    <Button
+                        label="Open in Maps"
+                        onClick={openExternalNavigation}
+                        style={styles.button}
+                    />
+                </View>
+            </View>
+        </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    centerContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    map: {
+        flex: 1,
+    },
+    loadingText: {
+        marginTop: 10,
+    },
+    cacheTile: {
+        position: "absolute",
+        bottom: 10,
+        left: 15,
+        right: 15,
+        backgroundColor: "#fff",
+        padding: 15,
+        borderRadius: 14,
+        shadowColor: "#0000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    cacheTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom:5,
+    },
+    detailsText: {
+        fontSize: 16,
+        fontWeight: "600",
+        marginBottom: 5,
+    },
+    buttonContainer: {
+        marginTop: 5,
+        alignItems: "center",
+    },
+});
 
 export default CacheNavigationScreen;
