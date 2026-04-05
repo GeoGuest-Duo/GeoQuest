7125bdab24d7a7f6736555b256f8d0b85b4d4c42
SCheriyamParambil <shaniyacp@outlook.com>
2026-04-05 20:26:25 +0100

    Added calculation logic to calculate the distance  between user and cache
    - Implemented Haversine formula to calculate distance in kilometres
    - Displayed the calculated distance on the screen
    - Distance can also be used to do proximity-based  cache logging

--- src/screens/homeTab/CacheNavigationScreen.js
+++ src/screens/homeTab/CacheNavigationScreen.js
@@ -4,6 +4,32 @@ import * as Location from "expo-location";      // Gets the user's GPS location
 import MapView, { Marker } from "react-native-maps";
 
 
+const calculateDistanceInKm = (startLat, startLong, endLat, endLong) => {
+    // GPS coordinates are in degrees - Js math functions need radians
+    const toRadians = (value) => (value * Math.PI) / 180;
+
+    // Stores earth's radius in KMs
+    const earthRadius = 6371;
+
+    // Calculates difference between to points 
+    const diffInLat = toRadians(endLat - startLat);
+    const diffInLong = toRadians(endLong - startLong);
+
+    // Using the Haversine formula - Calculates how curved distance works on earth 
+    const a =
+    Math.sin(diffInLat / 2) * Math.sin(diffInLat / 2) +
+    Math.cos(toRadians(startLat)) *
+    Math.cos(toRadians(endLat)) *
+    Math.sin(diffInLong / 2) *
+    Math.sin(diffInLong / 2);
+    
+    // Converts the value into an angle that represents distance on a sphere
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    // earthRadius x calculated angle
+    return earthRadius * c;
+};
+
 const CacheNavigationScreen = ({navigation, route}) => {
     // Initialisations ---------------------
     const { cache } = route.params;
@@ -59,6 +85,16 @@ const CacheNavigationScreen = ({navigation, route}) => {
     }, [location]) 
     
     // Handlers ----------------------------
+    // If location exists, calculate the distance 
+    const distanceInKm = location
+        ? calculateDistanceInKm(
+            location.latitude,
+            location.longitude,
+            cache.CacheLatitude,
+            cache.CacheLongitude,
+        )
+        : 0;
+    
     // Loading / error states --------------
     if (errorMessage) {
         return (
@@ -94,6 +130,9 @@ const CacheNavigationScreen = ({navigation, route}) => {
                     description={cache.CacheDescription}
                 />
             </MapView>
+            <View style={styles.distanceTile}>
+                <Text style={styles.distanceText}>Distance: {distanceInKm.toFixed(2)} KM</Text>
+            </View>
         </View>
     );
 };
@@ -102,8 +141,34 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    centerContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
     map: {
         flex: 1,
+    },
+    loadingText: {
+        marginTop: 10,
+    },
+    distanceTile: {
+        position: "absolute",
+        top: 20,
+        alignSelf: "center",
+        backgroundColor: "#fff",
+        paddingVertical: 8,
+        paddingHorizontal: 14,
+        borderRadius: 10,
+        shadowColor: "#0000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 4,
+    },
+    distanceText: {
+        fontSize: 16,
+        fontWeight: "600",
     }
 });
 
