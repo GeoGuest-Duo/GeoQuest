d0bccbcc1056211669002227c58f796920222bcc
SCheriyamParambil <shaniyacp@outlook.com>
2026-04-05 16:33:29 +0100

    Implemented cache selection tile with navigation and UI (#10)
    - Replaced the default marker popup with a custom tile

--- src/UI/icons.js
+++ src/UI/icons.js
@@ -6,11 +6,13 @@ const Home = () => <MaterialIcons name='home' size={30} />;
 const Events = () => <MaterialIcons name='local-activity' size={30} />;
 const Leaderboard  = () => <MaterialIcons name='emoji-events' size={30} />;
 const Profile = () => <MaterialIcons name='person' size={30} />;
+const KeyboardArrowRight = () => <MaterialIcons name='keyboard-arrow-right' size={30}/>
 
 // Compose
 Icons.Home = Home;
 Icons.Events = Events;
 Icons.Leaderboard = Leaderboard;
 Icons.Profile = Profile;
+Icons.KeyboardArrowRight = KeyboardArrowRight;
 
 export default Icons;
