cbfa0a30d26f37101ac0285e572b8e531774f630
Ayan-Derie1 <Ayanderie16@gmail.com>
2026-04-05 16:58:10 +0100

    spelling mistakes

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -1,30 +1,30 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../screens/homeTab/HomeScreen';
-import ViewCacheScreen from '../screens/homeTab/ViewCacheScreen';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/homeTab/HomeScreen";
+import ViewCacheScreen from "../screens/homeTab/ViewCacheScreen";
+import Icons from "../UI/Icons";
 
 const HomeStack = () => {
-    // Initialisations ---------------------
-    const Stack = createNativeStackNavigator();
+  // Initialisations ---------------------
+  const Stack = createNativeStackNavigator();
 
-    // View --------------------------------
-    return (
-        <Stack.Navigator
-            initialRouteName='HomeScreen'
-            screenOptions={{headerShown: false}}
-        >
-            <Stack.Screen
-                name="HomeScreen"
-                component={HomeScreen}
-                options={{ title: "Home" }}
-            />
+  // View --------------------------------
+  return (
+    <Stack.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ title: "Home" }}
+      />
 
-            <Stack.Screen
-                name="ViewCacheScreen"
-                component={ViewCacheScreen}
-                options={{ title: "View cache details " }}
-            />
-        </Stack.Navigator>
-    );
+      <Stack.Screen
+        name="ViewCacheScreen"
+        component={ViewCacheScreen}
+        options={{ title: "View cache details " }}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default HomeStack;
